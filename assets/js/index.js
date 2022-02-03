88b791d2c6837d4ba690c7a9936305ff3ab57d85
masterpranay1 <rajpranaychottu@gmail.com>
2022-02-03 12:05:15 +0530

    comments and  error fields to all input  added in html

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+// open with sign up page
+// if **sign in** pressed, go to sign in page( and change accordingly )
+// if **sign up** pressed, go to sign up page( and change accordingly )
+
+/* 
+    Sign up Page :
+    an object storing the name, email, password -> userDetails
+    1. Enter Name
+        check the length of name 
+    2. Enter Email
+        check for validation && check if exist already
+    3. Enter Password and confirm Password
+        check the password size and validation
+        match passwords
+    4. Check the checkbox
+        check if checked or not
+    5. Pressing the sign up button
+    6. Next And Prev
+        How to handle **next** and **prev** -> just update classes
+        ( name , email, password, checkbox )
+
+*/
